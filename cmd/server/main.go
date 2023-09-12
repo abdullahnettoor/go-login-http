f2945706c5ec877d396867048ca6a70ce7fdf762
abdullahnettoor <abdullahnettoor@gmail.com>
2023-09-12 22:14:40 +0530

    🖥️ Completed UI

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,23 +5,49 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+	http.FileServer(http.Dir("view/index.html"))
+	// io.WriteString(w, "Hello, Abdullah!\n")
 }
+
 func getLogin(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /login request\n")
 	io.WriteString(w, "Login, HTTP!\n")
+	file, err := os.Open("./view/login.html")
+	if err != nil {
+		fmt.Fprintf(w, "Error loading file: %s", err)
+		return
+	}
+	defer file.Close()
+
+	// Read the file contents
+	html, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Fprintf(w, "Error reading file: %s", err)
+		return
+	}
+
+	// Write the file contents to the response
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(html)
 }
 
 func main() {
+	// tmpl := template.Must(template.ParseGlob("*.html"))
 
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/login", getLogin)
+	// tmpl.ExecuteTemplate()
 
-	err := http.ListenAndServe(":3333", nil)
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir("./view")))
+	// mux.HandleFunc("/", getRoot())
+	mux.HandleFunc("/login", getLogin)
+
+	err := http.ListenAndServe(":3333", mux)
 
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("Server Closed")
